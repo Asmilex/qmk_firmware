6fe9d7a6e524f1c54283cf1ca0ff4d4da1d86651
Asmilex <andresmm@outlook.com>
2023-02-24 17:08:12 +0100

    Macro for muting microphone

--- keyboards/keychron/v2/v2_ansi_stm32l432_ec11/keymaps/asmichron_v2/keymap.c
+++ keyboards/keychron/v2/v2_ansi_stm32l432_ec11/keymaps/asmichron_v2/keymap.c
@@ -26,6 +26,10 @@ enum layers{
     _FN3
 };
 
+enum custom_keycodes {
+    MUTE_MIC,
+};
+
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
@@ -33,7 +37,7 @@ enum layers{
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_67(
-        KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
+        KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          MUTE_MIC,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_PAGE_UP,
         LT_CAP,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,           KC_PAGE_DOWN,
         SC_LSPO,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            SC_RSPC, KC_UP,
@@ -67,6 +71,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,           _______,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______, _______,
         _______, _______,  _______,                             _______,                            _______,  _______,  _______,  _______, _______, _______)
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MUTE_MIC:
+            if (record->event.pressed) {
+                add_mods(MOD_MASK_CTRL & MOD_MASK_SHIFT);
+                tap_code_delay(KC_M, 10);
+                del_mods(MOD_MASK_CTRL & MOD_MASK_SHIFT);
+            }
+            return false;
+            break;
+    }
+    return true;
+}
+
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
